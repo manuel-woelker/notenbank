38152d30518ae6ab5af3d18f0b8f2c270813174c
Manuel Woelker <manuel.woelker+jj@gmail.com>
2025-08-02 11:08:32 +0200

    fix(UI): use relative paths for built assets

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "", // use relative paths to allow hosting on subpaths
   plugins: [react()],
 })
